e26108fe6ec9dd5db3c9755a3907167e87901ba1
Kojo Gambrah <kgambrah@gmail.com>
2012-02-09 07:57:36 +0000

    Fixed various transformation files

--- etl/load_dw.sh
+++ etl/load_dw.sh
@@ -12,27 +12,41 @@ then
 	exit 2
 fi
 
+PRG="$0"
+
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+
+PRGDIR=`dirname "$PRG"`
+
 PDI_HOME="$1"
 DBUSER="$2"
 DBPASSWD="$3"
 DB_NAME="$4"
+
+
+
 
 # Recreate the motech data warehouse and run ETL scripts to populate it with data
 
 # 1. check for existing database
 
 # create database for dw
-mysqladmin -u"$DBUSER" -p"$DBPASSWD" drop "$DB_NAME"
-mysqladmin -u"$DBUSER" -p"$DBPASSWD" create "$DB_NAME"
+printf "%b" "Creating database (will drop existing database if present) \n"
+echo "drop database if exists $DB_NAME" | mysql -u"$DBUSER" -p"$DBPASSWD"
+echo "create database $DB_NAME" | mysql -u"$DBUSER" -p"$DBPASSWD"
 
-# create dw structure (sql) 
+# create dw schema (sql) 
 printf "%b" "Creating schema for database \n"
-mysql -u"$DBUSER" -p"$DBPASSWD" -D"$DB_NAME" < motech_dw_schema.sql
-
-# check for presence of source database
+mysql -u"$DBUSER" -p"$DBPASSWD" -D"$DB_NAME" < $PRGDIR/motech_dw_schema.sql
 
 #run ETL job 
-$PDI_HOME/./kitchen.sh -file=main_job.kjb -norep
-
-
-
+$PDI_HOME/kitchen.sh /file:`readlink -f $PRGDIR/main_job.kjb`  -norep
